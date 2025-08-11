1df78294470646d2c8c2efb36254f38df695c919
Lucas-Magalhaes-oficial <lucas.magalhaes.lms@gmail.com>
2025-08-11 17:14:21 -0300

    updt crud da prova
    finalização do cadastro e buscar quase finalizado

--- cadastro_usuario.php
+++ cadastro_usuario.php
@@ -16,7 +16,7 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
     $id_perfil = $_POST['id_perfil'];
     
     $sql="INSERT INTO usuario(nome,email,senha,id_perfil) VALUES (:nome,:email,:senha,:id_perfil)";
-    $stmt = pdo->prepare($sql);
+    $stmt = $pdo->prepare($sql);
     $stmt->bindParam(':nome',$nome);
     $stmt->bindParam(':email',$email);
     $stmt->bindParam(':senha',$senha);
@@ -36,10 +36,35 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Painel principal</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
     <h2>Cadastrar Usuario</h2>
     <form action="cadastro_usuario.php" method="POST">
+
+        <label for="nome">Nome:</label>
+        <input type="text" name="nome" id="nome" required>
+
+        <label for="email">Email:</label>
+        <input type="email" name="email" id="email" required>
+
+        <label for="senha">Senha:</label>
+        <input type="password" name="senha" id="senha" required>
+
+        <label for="id_perfil">Perfil</label>
+        <select type="id" name="id_perfil">
+            <option value="1">Administrador</option>
+            <option value="2">Secretaria</option>
+            <option value="3">Almoxarife</option>
+            <option value="4">Cliente</option>
+        </select>
+
+        <button type="submit">Salvar</button>
+        <button type="reset">Cancelar</button>
+    </form>
+    <a href="principal.php">Voltar</a>
+
+    <adress
 </body>
 </html>
 
